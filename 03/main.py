618b0e81017390ed1b7f0a44248536ca84570cf3
Grant Ramsay <seapagan@gmail.com>
2024-12-04 18:52:00 +0000

    day 3 slight refactor to reduce duplication

--- 03/main.py
+++ 03/main.py
@@ -7,12 +7,12 @@ from __future__ import annotations
 
 import re
 from pathlib import Path
-from typing import Generator, Literal
+from typing import Generator
 
 # This regex will find each proper 'mul(x,y)' string and return a list of each
-# pair as a tuple
-REGEX_PART1 = r"mul\((\d{1,3}),(\d{1,3})\)"
-REGEX_PART2 = r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)"
+# pair as a tuple. It will also match the `do()` and `don't()` for the second
+# part
+REGEX = r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)"
 
 
 # get the data in from file
@@ -22,39 +22,39 @@ def get_data() -> str:
         return file.read()
 
 
-def get_pairs(input_data: str, regex: str) -> Generator[tuple[int, int], None, None]:
-    """Return a GENERATOR with tuple(int,int)."""
-    for match in re.finditer(regex, input_data):
-        yield (int(match.group(1)), int(match.group(2)))
-
-
-def get_pairs_with_toggle(
-    input_data: str, regex: str
+def get_pairs(
+    input_data: str,
+    *,
+    use_toggle: bool = False,
 ) -> Generator[tuple[int, int], None, None]:
-    """Process input sequentially, toggling whether to handle `mul` pairs."""
-    toggle = True  # Start with handling enabled
-    for match in re.finditer(regex, input_data):
-        if match.group(0) == "do()":
-            toggle = True
-        elif match.group(0) == "don't()":
-            toggle = False
-        elif match.group(1) and toggle:  # A `mul(x, y)` and toggle is enabled
+    """Process input sequentially.
+
+    Optionally, use the toggle marks whether to handle or ignore `mul` pairs."""
+    toggle = True
+    for match in re.finditer(REGEX, input_data):
+        if use_toggle:
+            if match.group(0) == "do()":
+                toggle = True
+                continue
+            elif match.group(0) == "don't()":
+                toggle = False
+                continue
+
+        if match.group(1) and toggle:
             yield int(match.group(1)), int(match.group(2))
 
 
-def part1(data: str) -> int:
-    """Return the total for each 'mul()' pair."""
-    return sum(x * y for x, y in get_pairs(data, REGEX_PART1))
+def calculate(data: str, *, use_toggle: bool = False) -> int:
+    """Return the total for each 'mul()' pair.
 
-
-def part2(data: str) -> int:
-    """Return the total for each 'mul()' pair."""
-    return sum(x * y for x, y in get_pairs_with_toggle(data, REGEX_PART2))
+    Optionally, pay attention to the toggle commands.
+    """
+    return sum(x * y for x, y in get_pairs(data, use_toggle=use_toggle))
 
 
 data = get_data()
-part1_result = part1(data)
-part2_result = part2(data)
+part1_result = calculate(data)
+part2_result = calculate(data, use_toggle=True)
 
 print(f"Result for part1 is {part1_result}")  # for me is 159833790
-print(f"Result for part2 is {part2_result}")
+print(f"Result for part2 is {part2_result}")  # for me is 89349241
